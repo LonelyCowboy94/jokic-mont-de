6112f09f7ec93ec2f477ce4984b526a1c8dd2ddf
lonely_cowboy <nikola.jokic994@yahoo.co.uk>
2025-11-15 05:10:40 +0100

    delete unused inport

--- components/ui/CustomSlider/SliderPreview.tsx
+++ components/ui/CustomSlider/SliderPreview.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import styles from "./Slider.module.scss";
 import { Slide } from "./Slider";
-import { metadata } from "framer-motion/client";
+
 
 type SliderPreviewProps = {
   slides: Slide[];
